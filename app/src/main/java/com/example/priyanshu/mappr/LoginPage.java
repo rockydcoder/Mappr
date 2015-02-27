39e4ec74b3f39139f932ea7bf0cbcd6a21529571
Rakesh Sarangi <rakesh.rockyrocks.xxx@gmail.com>
2015-02-27 18:10:32 +0530

    Added UI for sign up page

--- app/src/main/java/com/example/priyanshu/mappr/LoginPage.java
+++ app/src/main/java/com/example/priyanshu/mappr/LoginPage.java
@@ -1,13 +1,15 @@
 package com.example.priyanshu.mappr;
 
+import android.app.Dialog;
 import android.content.Intent;
-import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -18,17 +20,18 @@ public class LoginPage extends ActionBarActivity implements View.OnClickListener
     private EditText userName;
     private EditText passWord;
     private CheckBox rememberPassword;
-    private Button login;
+    private Button login, signup;
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login_page);
         initialize();
 
         login.setOnClickListener(this);
+        signup.setOnClickListener(this);
 
     }
 
@@ -61,12 +64,55 @@ public class LoginPage extends ActionBarActivity implements View.OnClickListener
         passWord = (EditText)findViewById(R.id.passWord);
         rememberPassword = (CheckBox)findViewById(R.id.remember);
         login = (Button)findViewById(R.id.login);
+        signup = (Button) findViewById(R.id.singup);
 
     }
 
     @Override
     public void onClick(View view) {
-        Intent intent = new Intent(LoginPage.this, HomePage.class);
-        startActivity(intent);
+        switch(view.getId()) {
+            case R.id.login:
+                Intent intent = new Intent(LoginPage.this, HomePage.class);
+                startActivity(intent);
+                break;
+
+            case R.id.singup:
+                final Dialog dialog = new Dialog(this);
+                Button student, teacher, parent;
+
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                dialog.setContentView(R.layout.layout_dialog_signup);
+                dialog.setCanceledOnTouchOutside(true);
+
+                student = (Button) dialog.findViewById(R.id.student);
+                parent = (Button) dialog.findViewById(R.id.parent);
+                teacher = (Button) dialog.findViewById(R.id.teacher);
+
+                student.setOnClickListener(this);
+                teacher.setOnClickListener(this);
+                parent.setOnClickListener(this);
+
+                dialog.show();
+                break;
+
+            case R.id.parent:
+                Intent intent1 = new Intent(LoginPage.this, SignUpStudent.class);
+                intent1.putExtra("Button", 'p');
+                startActivity(intent1);
+                break;
+
+            case R.id.teacher:
+                Intent intent2 = new Intent(LoginPage.this, SignUpStudent.class);
+                intent2.putExtra("Button", 't');
+                startActivity(intent2);
+                break;
+
+            case R.id.student:
+                Intent intent3 = new Intent(LoginPage.this, SignUpStudent.class);
+                intent3.putExtra("Button", 's');
+                startActivity(intent3);
+                break;
+        }
+
     }
 }
